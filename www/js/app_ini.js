4406cec3a4f8b210189960fb91f6b83027ca23da
Booz Avila <raganaxi@gmail.com>
2017-01-26 00:30:29 -0600

    agregando google analitycs

--- www/js/app_ini.js
+++ www/js/app_ini.js
@@ -47,10 +47,46 @@ var callFailure = function(data) {
     function onDeviceReady() {
         initMap();
         gaPlugin = window.plugins.gaPlugin;
-        gaPlugin.init(successHandler, errorHandler, "UA-90947818-1", 10);
+        navigator.notification.confirm('A Dónde Ir en la Ciudad desea su permiso para recolectar datos de uso. No se recopilarán datos personales o de identificación del usuario.', permissionCallback, 'Attention', 'Allow,Deny');
+        
         //navigator.geolocation.getCurrentPosition(onSuccess, onError);
         //cordova.plugins.diagnostic.isWifiAvailable(callSuccess, callFailure);
     }
+    function permissionCallback (button) {
+                if (button === 1)
+                    gaPlugin.init(nativePluginResultHandler, nativePluginErrorHandler, "UA-90947818-1", 10);
+            }
+            function nativePluginResultHandler (result) {
+                //alert('nativePluginResultHandler - '+result);
+                console.log('nativePluginResultHandler: '+result);
+            }
+        
+            function nativePluginErrorHandler (error) {
+                //alert('nativePluginErrorHandler - '+error);
+                console.log('nativePluginErrorHandler: '+error);
+            }
+            
+            function TrackButtonClicked() {
+                gaPlugin.trackEvent( nativePluginResultHandler, nativePluginErrorHandler, "Button", "Click", "event only", 1);
+            }
+        
+            function VariableButtonClicked() {
+                // Set a dimension based on index and value. Make sure you have added a dimension in the GA dashboard to the
+                // default property for the passed in index, and your desired scope. GA allows up to 20 dimensions for a free account
+                gaPlugin.setVariable( nativePluginResultHandler, nativePluginErrorHandler, 1, "Purple");
+                // dimensions are are passed to the next event sent to GA. go ahead and fire off an event with the label (key) of your choice
+                // In this example, the label for custom dimension 1 will show up in the dashboard as "favoriteColor". This is much more efficent
+                // than the old custom variable method introduced in V1, (plus you get 20 free dimensions vs 5 free custom variables)
+                gaPlugin.trackEvent( nativePluginResultHandler, nativePluginErrorHandler, "event with variable", "set variable", "favoriteColor", 1);
+            }
+        
+            function PageButtonClicked() {
+                gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "some.url.com");
+            }
+            
+            function goingAway() {
+                gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
+            }
 
     // onSuccess Geolocation
     //
